0363d85712442dee4783aef505d012232b54d937
Caio Guedes <caio.cesar.g.souza@gmail.com>
2023-07-24 18:59:14 +0100

    [basics] Add JvmOverloads annotation

---
+++ basics/InteropTesting.java
@@ -0,0 +1,7 @@
+public class InteropTesting {
+    public static void main(String[] args) {
+        JvmAnnotationsKt.sum2(1, 2);
+        JvmAnnotationsKt.sum2(2);
+        JvmAnnotationsKt.sum2();
+    }
+}
---
+++ basics/JvmAnnotations.kt
@@ -0,0 +1,4 @@
+
+@JvmOverloads
+fun sum2(a: Int = 0, b: Int = 0) = a + b
+// Note! Generate Overloads methods for each default argument
